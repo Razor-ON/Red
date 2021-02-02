6137312e918c1d8ca29cbd960a3e1f7c97fb24c0
Razor-ON <78445602+Razor-ON@users.noreply.github.com>
2021-02-02 22:42:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -59,7 +59,8 @@ client.on('message', async message => {
             message.author.send(`You can only send <:hugs:805110056399274065> in <#${message.channel.id}>`);
             return;
         } else {
-            message.channel.setTopic(message.channel.topic + 1);
+            if(isNaN(message.channel.topic)) message.channel.setTopic('0');
+            else message.channel.setTopic(message.channel.topic + 1);
         };
     };
 });
